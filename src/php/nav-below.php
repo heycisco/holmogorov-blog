ba1f381adb0b61c974684fbc72c872bcfcc1bf33
heycisco <mail@starchenkov.pro>
2021-08-24 18:18:59 +0300

    112

--- src/php/nav-below.php
+++ src/php/nav-below.php
@@ -46,35 +46,7 @@
 			Наверх
 		</a>
 	</div>
-	<div class="center-2">
-		<?php
-		if(have_posts()) : while(have_posts()) : the_post();
-			$cats = wp_get_post_categories($post->ID);
-			if($cats) : foreach($cats as $cat) : $category = get_category($cat);
-			?>
-		<a href="<?php echo get_category_link($category->cat_ID); ?>" title="<?php echo $category->name ?>" class="btn-to-top">
-			<svg
-				aria-hidden="true"
-				focusable="false"
-				data-prefix="fas"
-				data-icon="chevron-up"
-				class="svg-inline--fa fa-chevron-up fa-w-14"
-				role="img"
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 448 512"
-			>
-				<path
-					fill="currentColor"
-					d="M240.971 130.524l194.343 194.343c9.373 9.373 9.373 24.569 0 33.941l-22.667 22.667c-9.357 9.357-24.522 9.375-33.901.04L224 227.495 69.255 381.516c-9.379 9.335-24.544 9.317-33.901-.04l-22.667-22.667c-9.373-9.373-9.373-24.569 0-33.941L207.03 130.525c9.372-9.373 24.568-9.373 33.941-.001z"
-				></path>
-			</svg>
-			В категорию
-		</a>
-		<?php
-		endforeach;endif;
-		endwhile;endif;
-		?>
-	</div>
+
 	<div class="right">
 		<?php
 		$next_post = get_next_post();
