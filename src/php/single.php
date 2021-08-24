9cf7f998d9f5860956a457b652b88d3e0b85be56
heycisco <mail@starchenkov.pro>
2021-08-24 11:44:05 +0300

    323

--- src/php/single.php
+++ src/php/single.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
-<main class="main-block__single single">
+<main class="main-block single-post" id="top">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
 <?php if ( comments_open() && ! post_password_required() ) { comments_template( '', true ); } ?>
 <?php endwhile; endif; ?>
-<footer class="footer">
+<div class="post-footer">
 <?php get_template_part( 'nav', 'below-single' ); ?>
-</footer>
+</div>
 </main>
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
