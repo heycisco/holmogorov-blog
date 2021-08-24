5b91082fa838e23dc6aa699388c69d7af5efc124
heycisco <mail@starchenkov.pro>
2021-08-24 13:50:32 +0300

    23

--- src/php/page.php
+++ src/php/page.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
 <main class="main-block page" id="top">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<section class="content">
 <article>
 <header class="header">
 <h1 class="entry-title"><?php the_title(); ?></h1>
 </header>
-<section class="content">
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 <?php the_content(); ?>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
-</section>
 </article>
+</section>
 <?php endwhile; endif; ?>
 </main>
 <?php get_sidebar(); ?>
