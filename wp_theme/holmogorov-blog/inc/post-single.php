3197077489ebaebe8f3ca1ccde61e1d6e69537ef
heycisco <mail@starchenkov.pro>
2021-08-24 12:17:55 +0300

    21

--- wp_theme/holmogorov-blog/inc/post-single.php
+++ wp_theme/holmogorov-blog/inc/post-single.php
@@ -1,10 +1,28 @@
+<section class="main-post-section">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<section class="single__article content">
-<header class="single__article__header">
-<h1>
-<?php the_title(); ?>
-</h1>
-</header>
+<div class="header">
+<div class="hwrapper">
+	<h2>
+	<?php the_title(); ?>
+	</h2>
+	<div class="post-meta">
+		<div class="date">
+			Опубликован: <span><?php echo get_the_date('n-j-Y'); ?></span>
+		</div>
+		<div class="tags">
+			Теги: 
+<?php
+$posttags = get_the_tags();
+if ($posttags) {
+  foreach($posttags as $tag) {
+    echo $tag->name . ' '; 
+  }
+}
+?>
+		</div>
+	</div>
+</div>
+</div>
 <div class="single__article__content">
 <?php if ( has_post_thumbnail() ) : ?>
 <a href="<?php $src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full', false ); echo esc_url( $src[0] ); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail(); ?></a>
@@ -12,5 +30,5 @@
 <?php the_content(); ?>
 </div>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
-</section>
 </article>
+</section>
