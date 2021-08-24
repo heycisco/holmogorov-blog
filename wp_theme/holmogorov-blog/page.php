fed16e0b7e06fe7ad0cbfc095bf56b471e1fcb0b
heycisco <mail@starchenkov.pro>
2021-08-24 13:45:40 +0300

    55

--- wp_theme/holmogorov-blog/page.php
+++ wp_theme/holmogorov-blog/page.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
-<main class="main-block__page page">
+<main class="main-block page" id="top">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <article>
 <header class="header">
 <h1 class="entry-title"><?php the_title(); ?></h1>
 </header>
-<section class="page__content">
+<section class="content">
 <?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
 <?php the_content(); ?>
 <div class="entry-links"><?php wp_link_pages(); ?></div>
