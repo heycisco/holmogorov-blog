9cdfd90cc7d78050fe958fc3012f003cee2947d8
heycisco <mail@starchenkov.pro>
2021-08-24 16:51:53 +0300

    23

--- src/php/inc/meta.php
+++ src/php/inc/meta.php
@@ -1,2 +1,5 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;1,300;1,400;1,600&family=Ruda&display=swap" rel="stylesheet">
